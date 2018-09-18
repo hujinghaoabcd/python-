74b7353d2a14a22660b290853fdb37581ea8965a
hujinghao <731409213@qq.com>
2018-09-18 11:36:09 +0800

    output and input

---
+++ output and input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# author:hao
+
+print("hello,word")
+print("hello", "tom", "jim", "jerry")
+print(300)
+print(100 + 300)
+print("100+300=", 100 + 300)
+
+name = input("请输入你的名字")
+print("hello", name)
+
+
+
