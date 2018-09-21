4bd19da5ac94d882af87cad01fdaf34a5595de73
hujinghao <731409213@qq.com>
2018-09-21 09:43:49 +0800

    循环2

--- Xunhuan.py
+++ Xunhuan.py
@@ -30,3 +30,19 @@ while n > 0:
 print(sums)
 
 # break 跳出循环
+n = 1
+while n < 100:
+    if n > 10:
+        break
+    print(n)
+    n = n + 1
+print("end")
+
+# continue语句会直接继续下一轮循环，后续的print()语句不会执行
+n = 0
+while n < 10:
+    n = n + 1
+    if n % 2 == 0:
+        continue
+    print(n)
+    
