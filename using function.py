100460607cfe18c754ff93f0c593412bebba3f64
hujinghao <731409213@qq.com>
2018-09-22 09:54:11 +0800

    调用函数

---
+++ using function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# author:hao
+
+# 调用求绝对值函数
+c = input('请输入求取绝对值的数字')
+a = int(c)
+s1 = abs(a)
+print("{0}的绝对值为:{1}".format(a, s1))
+
+# 调用help函数
+print(help(abs))
+
+# max函数
+print(max(1, 3, 4))
